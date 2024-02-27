1a4c85a53f86a61ed72f249104830f6f1f7fd236
javidannasib <javidan.code@gmail.comjavidan.code@gmail.comjavidan.code@gmail.com>
2024-02-27 12:53:06 +0400

    viewModel

--- Api/FlightViewModel.swift
+++ Api/FlightViewModel.swift
@@ -9,13 +9,18 @@ import UIKit
 
 class FlightViewModel {
     
-    func getList () {
+    var flightResponse : FlightResponse? = nil
+    
+    func getList (completipon: @escaping (Result<Void, NetworkError>)->Void) {
         let url = "https://65a7624794c2c5762da692dd.mockapi.io/api/v1/flights"
         NetworkManager.shared.fetchData(from: url, method: .get, body: nil) { (result : Result<FlightResponse, NetworkError>)  in
             switch result{
             case .success(let flightResponse) :
+                self.flightResponse = flightResponse
+                completipon(.success( () ))
                 break
             case .failure(let error) :
+                completipon(.failure(error))
                 break
             }
         }
--- Api/ViewController.swift
+++ Api/ViewController.swift
@@ -8,11 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let viewModel = FlightViewModel
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
+    func fetchData(){
+        viewModel.getList{result in
+            switch result {
+            case .success:
+                self.handleSucces()
+            case .failure(let error):
+                self.handleFailure(error: error)
+            }}
+    }
+    
+    func handleSucces() {
+        
+    }
+    
+    func handleFailure () {
+        print("network error: \(error)")
+    }
+ 
 
 }
 
